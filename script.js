78620ec875413f30b3d2f98e214357f5aa145f09
Jonatas Thomazini <112639333+JonatasThomazini@users.noreply.github.com>
2023-03-15 22:19:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+const teste = document.getElementById('nav')
+function toggleMenu (){
+   if (menu.style.display == 'block'){
+    menu.style.display = 'none';
+    
+   }else{
+    menu.style.display = 'block'
+    
+    
+   }
+}
+function toggleTeste(){
+   if(teste.style.display == 'none'){
+      teste.style.display = 'none'
+   }
+}
